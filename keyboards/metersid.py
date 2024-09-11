0456d30713807dce3c9beb99bf5810f314dd930a
soue27 <man.dozor@gmail.com>
2024-09-11 16:46:10 +0500

    Добавлен поиск по короткому номеру

--- keyboards/metersid.py
+++ keyboards/metersid.py
@@ -7,14 +7,12 @@ def metersid_kb(meter_list, counter) -> InlineKeyboardMarkup:
     """Функция создания клавиатуры для выбора процесса работы бота"""
     builder = InlineKeyboardBuilder()
     metersid = meter_list
-    print(metersid)
     count = counter
     if count == 0:
         builder.row(InlineKeyboardButton(text='Передайте информацию о счетчике администратору!', callback_data="0"))
         return builder.as_markup()
     # elif count < 7:
     if count == 1:
-        print(metersid[0], metersid[1])
         try:
             lst = metersid[0].split(',')
             if 'кв.' in metersid[0]:
@@ -27,7 +25,6 @@ def metersid_kb(meter_list, counter) -> InlineKeyboardMarkup:
         return builder.as_markup(one_time_keyboard=True)
     else:
         for item in metersid:
-            print(item[0], item[1])
             try:
                 lst = item[0].split(',')
                 if 'кв.' in item[0]:
